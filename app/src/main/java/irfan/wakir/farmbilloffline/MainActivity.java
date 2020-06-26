dc3075701f45aba37e9a1c91fd12161fa4782c7b
wakiryounus <wakiryounus007@gmail.com>
2020-06-26 06:11:47 -0700

    sss

--- app/src/main/java/irfan/wakir/farmbilloffline/MainActivity.java
+++ app/src/main/java/irfan/wakir/farmbilloffline/MainActivity.java
@@ -3,6 +3,7 @@ package irfan.wakir.farmbilloffline;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
         setContentView(R.layout.activity_main);
-        
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
